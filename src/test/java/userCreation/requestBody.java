91410b8daa9acd6cbbcf91c1ccca5b1198d3ae72
Swapnil Idawate <swapnilidawate103@gmail.com>
2024-08-25 12:36:52 +0530

    imports

---
+++ src/test/java/userCreation/requestBody.java
@@ -0,0 +1,20 @@
+package userCreation;
+import static io.restassured.RestAssured.*;
+import static io.restassured.matcher.RestAssuredMatchers.*;
+import static org.hamcrest.Matchers.*;
+
+import java.util.HashMap;
+
+import org.testng.annotations.Test;
+
+import io.restassured.http.ContentType;
+
+public class requestBody {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+
+	}
+
+}
