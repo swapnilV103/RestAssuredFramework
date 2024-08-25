8fc1ad450f3583a5137eb0cf8e8c48cdcb8dd995
Swapnil Idawate <swapnilidawate103@gmail.com>
2024-08-25 15:27:58 +0530

    added a new method for getting the max id from the database

--- src/test/java/userCreation/requestBody.java
+++ src/test/java/userCreation/requestBody.java
@@ -2,19 +2,61 @@ package userCreation;
 import static io.restassured.RestAssured.*;
 import static io.restassured.matcher.RestAssuredMatchers.*;
 import static org.hamcrest.Matchers.*;
-
+import io.restassured.response.Response;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 
 import org.testng.annotations.Test;
 
 import io.restassured.http.ContentType;
 
 public class requestBody {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	int maxId=0;
+	
+	
+	
+	
+	@Test
+	public void currentIDfromDatabase() {
+		Response response = get("http://localhost:3000/people");
+		List<Integer> ids = response.jsonPath().getList("id", Integer.class);
+        maxId = ids.stream().max(Integer::compare).orElse(0);
+      
+        
 		
-
+			
 	}
+
+	@Test (dependsOnMethods= {"currentIDfromDatabase"})
+	void createUserUsingHashMap() {
+		
+		HashMap map = new HashMap();
+		
+		map.put("id", maxId+1);
+		map.put("name", "swapnil");
+		map.put("location", "baramati");
+		map.put("phone", "9047463323");
+		
+		String arr[] = {"Python", "Java"};
+		//System.out.print(Arrays.toString(arr));
+		map.put("courses", arr);
+		
+		
+		given()
+			.contentType("application/json")
+			.body(map)
+			
+		.when()
+			.post("http://localhost:3000/people")
+			
+		.then()
+			.log().all();
+		
+		
+	}
+	
+	
+	
 
 }
