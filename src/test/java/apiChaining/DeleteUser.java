cb920d99ee8b3316162c879512771ea54adc0ccd
Swapnil Idawate <swapnilidawate103@gmail.com>
2024-09-09 00:37:00 +0530

    delete user for api chaining suing IContextUser interface

---
+++ src/test/java/apiChaining/DeleteUser.java
@@ -0,0 +1,36 @@
+package apiChaining;
+import static io.restassured.RestAssured.*;
+import static io.restassured.matcher.RestAssuredMatchers.*;
+import static org.hamcrest.Matchers.*;
+import io.restassured.response.Response;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
+import org.json.JSONObject;
+import org.testng.ITestContext;
+import org.testng.annotations.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+
+public class DeleteUser {
+	
+	
+	@Test
+	void Delete_the_User(ITestContext context) {
+		
+		int id = (Integer) context.getSuite().getAttribute("user_id");
+		
+		given()
+			.contentType("application/json")
+			.header("Authorization","Bearer 206250362681fdc8c6e36db62cbd1328fe5ed3a399960867c23ea1016d596f78")
+			.pathParam("id", id)
+		.when()
+			.delete("https://gorest.co.in/public/v2/users/{id}")
+		.then()
+			.statusCode(204)
+			.log().all();
+	}
+
+}
